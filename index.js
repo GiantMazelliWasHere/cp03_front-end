e6233773f2388931796be869d76afd3d1b107642
eduardo <eduardo.mazelli@gmail.com>
2024-05-15 17:53:44 -0300

    mudanças no script

--- index.js
+++ index.js
@@ -1,16 +1,74 @@
-import OpenAI from "openai";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+  
+        const API_KEY = "AIzaSyBTEr33cjlO6ex7pRL2AJFdALavVGS5oKo";
+  
+        const genAI = new GoogleGenerativeAI(API_KEY);
+  
+        const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-const openai = new OpenAI({
-    organization:"org-qzsU9lbW27jBN1GWIXz96VVS",
-    apiKey:""
-});
+        let messages = [];
 
-const chatCompletion = await openai.chat.completions.create({
-    model:"gpt-3.5-turbo",
-    messages:[{"role":"user", "content":"Quem é você?"}]
-})
+        const form = document.querySelector("form");
+        const message = document.getElementById("message");
+        const chatlog = document.getElementById("chat-log");
 
-console.log(chatCompletion.choices[0].message)
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            
+            async function run() {
+                const prompt = message.value;
+
+                messages.push(prompt);
+                
+                message.value = "";
+
+                console.log(messages);
+
+                const messageElementDiv = document.createElement("div");
+                messageElementDiv.classList.add("message");
+                messageElementDiv.classList.add("message--sent");
+                messageElementDiv.innerHTML = `<div class="message__text">${prompt}</div>`;
+
+                chatlog.appendChild(messageElementDiv);
+
+                const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+                
+                const chat = model.startChat({
+                    history: [
+                        {
+                            role: "user",
+                            parts: [{ text: prompt }],
+                        },
+                        {
+                            role: "model",
+                            parts: [{ text: 'Thinking...' }],
+                        },
+                    ],
+                    generationConfig: {
+                        maxOutputTokens: 100,
+                    },
+                });
+                
+                const waitElementDiv = document.createElement("div");
+                waitElementDiv.classList.add("message");
+                waitElementDiv.classList.add("message--received");
+                waitElementDiv.innerHTML = `<div class="message__text">Thinking...</div>`;
+
+                chatlog.appendChild(waitElementDiv);
+
+                const msg = prompt;
+              
+                const result = await chat.sendMessage(msg);
+                const response = await result.response;
+                const text = response.text();
+
+                const botElementDiv = document.createElement("div");
+                botElementDiv.classList.add("message");
+                botElementDiv.classList.add("message--received");
+                botElementDiv.innerHTML = `<div class="message__text">${text}</div>`;
+
+                chatlog.appendChild(botElementDiv);
+              }
+              
+            run();
+        })
